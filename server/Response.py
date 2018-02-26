bb7a91daf4daa369ddef64d87a7e806cfe634a63
Pavel <grigorevpv@gmail.com>
2018-02-26 23:41:22 +0300

    made some changes in responce class

--- server/Response.py
+++ server/Response.py
@@ -3,39 +3,37 @@ from server.Header import *
 
 class Response(object):
 
-    def __init__(self, status_line: StatusLine, headers: Header, message: str, connection):
+    def __init__(self, status_line: StatusLine, headers: Header, message: str, connection, loop):
 
         self._status_line = status_line
         self._headers = headers
         self._message = message
         self._connection = connection
+        self._loop = loop
 
     @property
     def status_line(self) -> StatusLine:
-        return self.status_line
+        return self._status_line
 
     @property
     def headers(self) -> Header:
-        return self.headers
+        return self._headers
 
     @property
     def message(self) -> str:
-        return self.message
+        return self._message
 
     @property
     def connection(self):
         return self._connection
 
-    @classmethod
-    def answer(cls):
-        status_line: StatusLine = cls.status_line
-        headers: Header = cls.headers
-        message: str = cls.message
+    async def answer(self):
+        status_line: StatusLine = self.status_line
+        headers: Header = self.headers
+        message: str = self.message
         headers.content_length = len(message)
-        connection = cls.connection
+        data = status_line.get_status_line() + headers.get_headers() + b"\r\n" + message.encode("utf-8")
 
-        connection.send(status_line.get_status_line())
-        connection.send(headers.get_headers())
-        connection.send(b"\r\n")
-        connection.send(message.encode("utf-8"))
+        await self._loop.sock_sendall(self._connection, data)
+
 

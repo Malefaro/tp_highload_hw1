37bcf3c25286b91a47623a932e34a35ebb13541e
Pavel <grigorevpv@gmail.com>
2018-02-27 00:26:05 +0300

    added async read file function and send file function

--- server/http_parser.py
+++ server/http_parser.py
@@ -1,6 +1,20 @@
 from server.StatusLine import *
 from server.Header import *
 from server.Response import *
+import urllib.request
+from os.path import splitext, getsize
+
+types = {
+    ".js": "application/javascript",
+    ".jpeg": "image/jpeg",
+    ".jpg": "image/jpeg",
+    ".html": "text/html",
+    ".png": "image/png",
+    ".gif": "image/gif",
+    ".css": "text/css",
+    ".txt": "text/plain",
+    ".swf": "application/x-shockwave-flash",
+}
 
 
 async def send_error(conn, status_code, loop):
@@ -10,7 +24,50 @@ async def send_error(conn, status_code, loop):
     await response.answer()
 
 
-async def parse(conn, addr, pid, ROOT_DIR, loop):
+async def send_answer(conn, status_code, all_data, loop):
+    headers = Header(content_length=all_data)
+    status_line = StatusLine(status_code)
+    response = Response(status_line, headers, status_line.reason_phrese, conn, loop)
+    await response.answer()
+
+
+async def read_file(file, loop, executor):
+    with open(file, "rb") as f:
+        data = await loop.run_in_executor(executor, f.read)
+        return data
+
+
+async def send_file(conn, addr, method, root_dir, loop, executor):
+    file_addr = urllib.request.unquote(addr)
+    file_addr = file_addr.split("?")[0]
+    file_addr = (root_dir or '.') + file_addr
+    filetype = splitext(file_addr)[1]
+    if '..' in file_addr:
+        send_error(conn, 404, loop)
+    else:
+        try:
+            data = await read_file(file_addr, loop, executor)
+            status = 200
+            type = types[filetype]
+            all_data = getsize(file_addr)
+            send_answer(conn, status, all_data, type)
+            if method != "HEAD":
+                await loop.sock_sendall(conn, data)
+            conn.close()
+        except FileNotFoundError:
+            if "index.html" in file_addr:
+                send_error(conn, 403, loop)
+            else:
+                send_error(conn, 404, loop)
+        except IsADirectoryError:
+            if addr[-1] == '/':
+                addr += "index.html"
+            else:
+                addr += "/index.html"
+            send_file(conn, addr, method, root_dir, loop, executor)
+
+
+async def parse(conn, addr, pid, ROOT_DIR, loop, executor):
     data = b""
 
     while not b"\r\n" in data:
@@ -30,13 +87,10 @@ async def parse(conn, addr, pid, ROOT_DIR, loop):
     request = request.split("\r\n", 1)[0]
     print(request.split(" ", 2))
     if len(request.split(" ", 2)) < 3:
-        # senderror(conn, 404)
-        print("404")
+        send_error(conn=conn, status_code=404, loop=loop)
     else:
         method, address, protocol = request.split(" ", 2)
         if method in ("GET", "HEAD"):
-            print("method is get or head")
-            # sendfile(conn, address, method, ROOT_DIR)
+            await send_file(conn, address, method, ROOT_DIR, loop, executor)
         else:
-            print(405)
             await send_error(conn=conn, status_code=405, loop=loop)

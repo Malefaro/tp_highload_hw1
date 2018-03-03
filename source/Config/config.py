99c109bb4233c28f8dda323bcb1a3761e90a50c2
grigorevpv <grigorevpv@gmail.com>
2018-03-03 14:32:01 +0300

    made config class

---
+++ source/Config/config.py
@@ -0,0 +1,25 @@
+class Config(object):
+
+    def __init__(self, port=80, cpu_count=2,
+                 threads=256, root_dir='/var/www/html'):
+
+        self._threads = threads
+        self._port = port
+        self._cpu_count = cpu_count
+        self._root_dir = root_dir
+
+    @property
+    def threads(self) -> int:
+        return self._threads
+
+    @property
+    def port(self) -> int:
+        return self._port
+
+    @property
+    def cpu_count(self):
+        return self._cpu_count
+
+    @property
+    def root_dir(self):
+        return self.root_dir

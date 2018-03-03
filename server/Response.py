be544b995f0a3d2e70e6af754730a77204d853e7
grigorevpv <grigorevpv@gmail.com>
2018-03-03 16:50:49 +0300

    rewrited response class

--- server/Response.py
+++ server/Response.py
@@ -1,15 +1,13 @@
-from server.StatusLine import *
+from server.StatusLine import StatusLine
 from server.Header import *
 
 class Response(object):
 
-    def __init__(self, status_line: StatusLine, headers: Header, message: str, connection, loop):
+    def __init__(self, status_line: StatusLine, headers: Header, message: str):
 
         self._status_line = status_line
         self._headers = headers
         self._message = message
-        self._connection = connection
-        self._loop = loop
 
     @property
     def status_line(self) -> StatusLine:
@@ -23,17 +21,14 @@ class Response(object):
     def message(self) -> str:
         return self._message
 
-    @property
-    def connection(self):
-        return self._connection
 
-    async def answer(self):
+    def answer(self) -> bytes:
         status_line: StatusLine = self.status_line
         headers: Header = self.headers
         message: str = self.message
         headers.content_length = len(message)
         data = status_line.get_status_line() + headers.get_headers() + b"\r\n" + message.encode("utf-8")
 
-        await self._loop.sock_sendall(self._connection, data)
+        return data
 
 

7ab5359a8a7611c8e86c2e8e79cbed668211f5bb
grigorevpv <grigorevpv@gmail.com>
2018-03-03 20:04:57 +0300

    rewited responce

--- server/Response.py
+++ server/Response.py
@@ -1,13 +1,14 @@
 from server.StatusLine import StatusLine
 from server.Header import *
 
+
 class Response(object):
 
-    def __init__(self, status_line: StatusLine, headers: Header, message: str):
+    def __init__(self, status_line: StatusLine, headers: Header, message: bytes = None):
 
         self._status_line = status_line
         self._headers = headers
-        self._message = message
+        self._message = b"" if message is None else message
 
     @property
     def status_line(self) -> StatusLine:
@@ -18,16 +19,16 @@ class Response(object):
         return self._headers
 
     @property
-    def message(self) -> str:
+    def message(self) -> bytes:
         return self._message
-
 
     def answer(self) -> bytes:
         status_line: StatusLine = self.status_line
         headers: Header = self.headers
-        message: str = self.message
+        message: bytes = self.message
         headers.content_length = len(message)
-        data = status_line.get_status_line() + headers.get_headers() + b"\r\n" + message.encode("utf-8")
+        data = status_line.get_status_line() + headers.get_headers() + b"\r\n"
+        data += message
 
         return data
 

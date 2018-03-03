6986fdf3d5d8358cb021f988893b26d8ef246b08
Pavel <grigorevpv@gmail.com>
2018-03-04 00:53:39 +0300

    fix response

--- server/Response.py
+++ server/Response.py
@@ -4,10 +4,11 @@ from server.Header import *
 
 class Response(object):
 
-    def __init__(self, status_line: StatusLine, headers: Header, message: bytes = None):
+    def __init__(self, status_line: StatusLine, headers: Header, message: bytes = None, content_length: int = None):
 
         self._status_line = status_line
         self._headers = headers
+        self._content_length = 0 if content_length is None else content_length
         self._message = b"" if message is None else message
 
     @property
@@ -22,11 +23,15 @@ class Response(object):
     def message(self) -> bytes:
         return self._message
 
+    @property
+    def content_length(self):
+        return self._content_length
+
     def answer(self) -> bytes:
         status_line: StatusLine = self.status_line
         headers: Header = self.headers
         message: bytes = self.message
-        headers.content_length = len(message)
+        headers.content_length = self.content_length
         data = status_line.get_status_line() + headers.get_headers() + b"\r\n"
         data += message
 
